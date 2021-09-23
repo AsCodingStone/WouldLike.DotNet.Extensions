b9bea375373e602dcb8caf2b108fec0a5c030303
Pierre Large <pierre@plarge.com>
2021-09-23 17:54:59 +0200

    :star: new: DateTime.TrimMillisecond extension method

---
+++ code/sources/Plarge.DotnetExtended/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Plarge.DotnetExtended
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime TrimMillisecond(this DateTime input)
+        {
+            return new DateTime(
+                input.Year,
+                input.Month,
+                input.Day,
+                input.Hour,
+                input.Minute,
+                input.Second,
+                millisecond: 0,
+                input.Kind);
+        }
+    }
+}
---
+++ code/tests/Plarge.DotnetExtended.Tests/DateTimeExtensionTests.cs
@@ -0,0 +1,26 @@
+﻿using NFluent;
+using NUnit.Framework;
+using System;
+
+namespace Plarge.DotnetExtended.Tests
+{
+    [TestFixture]
+    public class DateTimeExtensionTests
+    {
+        [Test]
+        public void ToRename()
+        {
+            const int year = 2021;
+            const int month = 9;
+            const int day = 21;
+            const int hour = 12;
+            const int minute = 43;
+            const int secoond = 54;
+            const int millisecond = 234;
+            var initialDateTime = new DateTime(year, month, day, hour, minute, secoond, millisecond);
+
+            var result = initialDateTime.TrimMillisecond();
+            Check.That(result.Millisecond).IsZero();
+        }
+    }
+}
