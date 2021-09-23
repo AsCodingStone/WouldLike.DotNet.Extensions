e3243f1084240104ef2c9dc1942f4d1d62c9f66e
Pierre Large <pierre@plarge.com>
2021-09-23 16:29:01 +0200

    :star: new: string.Truncate extension method

---
+++ code/sources/Plarge.DotnetExtended/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Plarge.DotnetExtended
+{
+    public static class StringExtensions
+    {
+        public static string Truncate(this string source, int length)
+        {
+            if (source.Length < length)
+                return source;
+
+            source = source.Substring(0, length);
+            return source;
+        }
+    }
+}
---
+++ code/tests/Plarge.DotnetExtended.Tests/StringExtensionTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using NFluent;
+
+namespace Plarge.DotnetExtended.Tests
+{
+    [TestFixture]
+    public class StringExtensionTests
+    {
+        [Test]
+        public void ShouldTruncateWhenStringIsLongerThanExpectedLength()
+        {
+            const string input = "This is a sample";
+            const string expected = "This is a ";
+            const int expectedLenght = 10;
+            var result = input.Truncate(expectedLenght);
+            Check.That(result).IsEqualTo(expected);
+            Check.That(result.Length).IsEqualTo(expectedLenght);
+        }
+
+
+        [Test]
+        public void ShouldNotTruncateWhenStringIsSmallerThanExpectedLength()
+        {
+            const string input = "This is a sample";
+            const string expected = "This is a sample";
+            const int expectedLenght = 200;
+            var initialLenght = input.Length;
+            var result = input.Truncate(expectedLenght);
+            Check.That(result).IsEqualTo(expected);
+            Check.That(result.Length).IsEqualTo(initialLenght);
+        }
+    }
+}
