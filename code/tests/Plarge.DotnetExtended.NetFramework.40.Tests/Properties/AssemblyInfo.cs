842afe7b72772bc85884196adf2eaabe375151a6
Pierre Large <pierre@plarge.com>
2021-09-23 17:56:03 +0200

    test: .NET Framework 4.0

---
+++ code/tests/Plarge.DotnetExtended.NetFramework.40.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Plarge.DotnetExtended.NetFramework.40.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Plarge.DotnetExtended.NetFramework.40.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("73bf1ec9-f44e-4948-9afb-ea5d3aaaa0da")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
